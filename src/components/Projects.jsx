5494aab3306d128b01fe075d32c716d6943136e5
Tarun-Pantangi <141355012+Tarun-Pantangi@users.noreply.github.com>
2024-08-24 01:08:28 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,34 +21,29 @@ const Projects = () => {
             transition={{ duration: 0.5 }}
             whileHover={{ scale: 1.05 }}
             key={project.id}
-            className="group relative overflow-hidden rounded-3xl"
+            className="relative overflow-hidden rounded-3xl"
           >
             <motion.img
               whileHover={{ scale: 1.1 }}
               src={project.image}
               alt={project.name}
-              className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
+              className="h-full w-full object-cover transition-transform duration-500"
             />
-            <motion.div
-              initial={{ opacity: 0 }}
-              whileHover={{ opacity: 1 }}
-              transition={{ duration: 0.5 }}
-              className="absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100"
-            >
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-white opacity-100 backdrop-blur-lg transition-opacity duration-500">
               <h3 className="mb-2 text-xl">{project.name}</h3>
               <p className="mb-12 p-4">{project.description}</p>
               <a
                 href={project.githubLink}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300"  
+                className="rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300"
               >
                 <div className="flex items-center">
                   <span>View on GitHub</span>
                   <MdArrowOutward />
                 </div>
               </a>
-            </motion.div>
+            </div>
           </motion.div>
         ))}
       </div>
